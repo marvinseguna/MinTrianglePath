7ec162f5361e2e999057d6188ed6f23025c96286
Marvin Seguna <mseguna1@gmail.com>
2018-06-21 01:00:01 +0200

    Initial Commit

---
+++ MinTrianglePath/src/main/main.java
@@ -0,0 +1,106 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class main 
+{
+	private static Scanner reader;
+	
+	public static void main( String args[] )
+	{
+		reader = new Scanner( System.in );
+		int choice = askForUserInput();
+		String bestPath = "";
+		switch( choice ) 
+		{
+			case 1: 
+				bestPath = readCLI();
+				break;
+			case 2: 
+				bestPath = readFile();
+				break;
+			default: 
+				System.out.println( "Exiting.." );
+				break;
+		}
+		System.out.println( bestPath );
+		reader.close();
+	}
+	
+	public static int askForUserInput()
+	{
+		// Give user selection of input
+		System.out.println( "MINIMUM/MAXMIMUM TRIANGLE PATHS!" );
+		System.out.println( "Choose:" );
+		System.out.println( "1) Input through CLI" );
+		System.out.println( "2) Input through file" );
+		System.out.println( "3) Exit" );
+		
+		// Read input
+		int choice = reader.nextInt();
+		
+		// Return user choice
+		return choice;
+	}
+	
+	public static String readCLI()
+	{
+		//Read user input in string
+		String userInput = "";
+		
+		// Change user input to upper case to match EOF even if lower case
+		while( !userInput.toUpperCase().equals( "EOF" ) )
+		{
+			userInput = reader.nextLine();
+			// Begin processing user input here
+			System.out.println( userInput );
+		}
+		
+		// Return answer
+		return "";
+	}
+	
+	public static String readFile()
+	{
+		//Read user input in string
+		System.out.println( "Enter path of file:" );
+		String file = reader.next();
+		
+		// Check if file exists
+		File f = new File( file );
+		if( f.exists() && !f.isDirectory() )
+		{
+			try
+			{
+				FileReader fr = new FileReader( f );
+				BufferedReader br = new BufferedReader( fr );
+				String line;
+				
+				while( ( line = br.readLine() ) != null )
+				{
+					// Begin processing user input here
+					System.out.println( line );
+				}
+				
+				// Close all readers
+				fr.close();
+				br.close();
+			}
+			catch( IOException e ) 
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			System.out.println( "File provided is not valid! Exiting.." );
+		}
+		
+		// Return answer
+		return "";
+	}
+}
